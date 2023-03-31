e8ca40595a9b434234f18d880b077702762886fa
Najam Ul Hassan <najmulhassan721@gmail.com>
2023-03-31 22:08:25 +0500

    added the nav menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 import logo from "../images/logo.svg";
+import home from "../images/home-icon.svg";
+import search from "../images/search-icon.svg";
+import watchlist from "../images/watchlist-icon.svg";
+import originals from "../images/original-icon.svg";
+import movies from "../images/movie-icon.svg";
+import series from "../images/series-icon.svg";
 
 function Header(props) {
   return (
     <Nav>
       <Logo src={logo} />
+      <NavMenu>
+        <a href="/home">
+          <img src={home} alt="HOME" />
+          <span>HOME</span>
+        </a>
+        <a href="/search">
+          <img src={search} alt="SEARCH" />
+          <span>SEARCH</span>
+        </a>
+
+        <a href="/watchlist">
+          <img src={watchlist} alt="WATCHLIST" />
+          <span>WATCHLIST</span>
+        </a>
+        <a href="/originals">
+          <img src={originals} alt="ORIGINALS" />
+          <span>ORIGINALS</span>
+        </a>
+
+        <a href="/movies">
+          <img src={movies} alt="MOVIES" />
+          <span>MOVIES</span>
+        </a>
+        <a href="/series">
+          <img src={series} alt="SERIES" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
       <LoginButton>Login</LoginButton>
     </Nav>
   );
@@ -44,5 +78,44 @@ const LoginButton = styled.a`
     background-color: #f9f9f9;
     color: #000;
     border-color: transparent;
+  }
+`;
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+    img {
+      height: 20px;
+    }
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+      &:after {
+        content: "";
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+      }
+    }
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
+    }
   }
 `;
